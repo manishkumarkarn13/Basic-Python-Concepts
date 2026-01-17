b1587eba969388778cd032fbea14dbffcac10434
manishkumarkarn13 <manishkumarkarn13@gmail.com>
2026-01-18 00:31:57 +0530

    ASSIGNMENT 1

---
+++ Coding Exercise.py
@@ -0,0 +1,17 @@
+a=input("Enter a 1st number from User")
+b=input("Enter a 2nd number from User")
+
+Addition =(int(a)+int(b))
+Subtraction =(int(a)-int(b))
+Multiplication=(int(a)*int(b))
+Division=(int(a)/int(b))
+
+
+print("Addition",Addition)
+print("Subtraction",Subtraction)
+print("Multiplication",Multiplication)
+print("Division",Division)
+
+
+
+
---
+++ Task 2Create a Personalized Greeting.py
@@ -0,0 +1,9 @@
+from tkinter.font import nametofont
+
+Name = input("Enter your First name ")
+Last = input("Enter your Last name ")
+
+print("Hello", Name+Last, "welcome to python program")
+
+github config -- global user.name "manishkumarkarn13"
+
