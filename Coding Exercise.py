dfb20239f89bb1da9174e0811d61adf208016481
manishkumarkarn13 <manishkumarkarn13@gmail.com>
2026-01-26 21:19:42 +0530

    Update Coding Exercise.py

--- Coding Exercise.py
+++ Coding Exercise.py
@@ -12,12 +12,8 @@ print("Subtraction",Subtraction)
 print("Multiplication",Multiplication)
 print("Division",Division)
 
-Enter a 1st number from User5
-Enter a 2nd number from User5
-Addition 10
-Subtraction 0
-Multiplication 25
-Division 1.0
+
+
 
 
 
