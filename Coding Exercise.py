bc53e260eb339011fa5d5473a343c2d09e1a507d
manishkumarkarn13 <manishkumarkarn13@gmail.com>
2026-01-26 20:55:36 +0530

    Update Coding Exercise.py
    take the input of numbers as int

--- Coding Exercise.py
+++ Coding Exercise.py
@@ -1,16 +1,17 @@
-a=input("Enter a 1st number from User")
-b=input("Enter a 2nd number from User")
+a=int(input("Enter a 1st number from User"))
+b=int(input("Enter a 2nd number from User"))
 
-Addition =(int(a)+int(b))
-Subtraction =(int(a)-int(b))
-Multiplication=(int(a)*int(b))
-Division=(int(a)/int(b))
+Addition =a+b
+Subtraction =a-b
+Multiplication=a*b
+Division=a/b
 
 
 print("Addition",Addition)
 print("Subtraction",Subtraction)
 print("Multiplication",Multiplication)
 print("Division",Division)
+
 
 
 
